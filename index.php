b296de3ca8853ed25c87442d7e6563a48589336e
Stanislaw <stashupert@gmail.com>
2022-04-01 11:32:08 +0200

    Pierwszy update projektu

---
+++ index.php
@@ -0,0 +1,52 @@
+<html !DOCTYPE>
+    <head>
+        <title>Tłumaczenie i Translacja </title>
+        <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/5.0.2/js/bootstrap.min.js"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,500&display=swap" rel="stylesheet"> 
+        <link rel='stylesheet' href="./css/styles.css">
+    </head>
+    <body>
+        <!-- <navbar  id="header" class='clearfix '> -->
+            <!-- <div class="col-xl-6  mt-2 text-xl-center float-start clearfix"><img class="col-4 clearfix w-10" src="./image/Logo.png"></div> -->
+            <!-- <div id="menuBar"> -->
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <section class="top-nav">
+                    <div>
+                        <img id="logo" src="./image/Logo.png">
+                    </div>
+                    <input id="menu-toggle" type="checkbox" />
+                    <label class='menu-button-container' for="menu-toggle">
+                    <div class='menu-button'></div>
+                </label>
+                    <ul class="menu">
+                    <li>Strona główna</li>
+                    <li>Oferta</li>
+                    <li>Cennik</li>
+                    <li>O nas</li>
+                    <li>Five</li>
+                    </ul>
+                </section>
+                <!-- <div id="mainSite" class="col-xl-2 col-lg-2 float-start mt-xl-5 text-xl-center clearfix">Strona główna</div>
+                <div id="offer" class="col-xl-1 float-start mt-xl-5 text-xl-center clearfix " >Oferta</div>
+                <div id="prices" class="col-xl-1 float-start mt-xl-5 text-xl-center clearfix" >Cennik</div>
+                <div id="aboutUs" class="col-xl-1 float-start mt-xl-5 text-xl-center clearfix" >O nas</div> -->
+            <!-- </div> -->
+        <!-- </navbar> -->
+        <!-- <div id="container" class='clearfix'>
+            <div class=" col-xl-10 offset-xl-1  text-xl-center"></div>
+        </div>
+        
+        
+        <footer class="col-xl-10 offset-xl-1  text-xl-center clearfix"></footer> -->
+
+    </body>
+            </html>
+<?php
+    echo '
+    ';
+?>
---
+++ menu.php
@@ -0,0 +1,34 @@
+<html !DOCTYPE>
+    <head>
+        <title>Tłumaczenie i Translacja </title>
+        <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/5.0.2/js/bootstrap.min.js"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,500&display=swap" rel="stylesheet"> 
+        <link rel='stylesheet' href="./css/styles.css">
+    </head>
+    <body>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+        <section class="top-nav">
+            <div>
+            Logo Here
+            </div>
+            <input id="menu-toggle" type="checkbox" />
+            <label class='menu-button-container' for="menu-toggle">
+            <div class='menu-button'></div>
+        </label>
+            <ul class="menu">
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+            <li>Four</li>
+            <li>Five</li>
+            </ul>
+        </section>
+
+        <h2>Resize window to collapse menu</h2>
+    </body>
+</html>
