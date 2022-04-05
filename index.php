315dc4bcf75f3783abf16f8240d61305502db218
Stanislaw <stashupert@gmail.com>
2022-04-05 20:17:11 +0200

    Dokonano drugiego updatu

--- index.php
+++ index.php
@@ -14,39 +14,63 @@
         <!-- <navbar  id="header" class='clearfix '> -->
             <!-- <div class="col-xl-6  mt-2 text-xl-center float-start clearfix"><img class="col-4 clearfix w-10" src="./image/Logo.png"></div> -->
             <!-- <div id="menuBar"> -->
-                <meta name="viewport" content="width=device-width, initial-scale=1">
                 <section class="top-nav">
                     <div>
                         <img id="logo" src="./image/Logo.png">
                     </div>
                     <input id="menu-toggle" type="checkbox" />
                     <label class='menu-button-container' for="menu-toggle">
-                    <div class='menu-button'></div>
-                </label>
-                    <ul class="menu">
-                    <li>Strona główna</li>
-                    <li>Oferta</li>
-                    <li>Cennik</li>
-                    <li>O nas</li>
-                    <li>Five</li>
+                        <div class='menu-button'></div>
+                    </label>
+                    <ul class="menu col-6">
+                        <li class="col-xxl-3 mt-xxl-5 pt-xxl-5 col-xl-4 mt-xl-5 pt-xl-5 col-lg-5 mt-lg-5 pt-lg-5 col-md-5 mt-md-5 pt-md-5   h4 ">Strona główna</li>
+                        <li class="col-xxl-2 mt-xxl-5 pt-xxl-5 col-xl-2 mt-xl-5 pt-xl-5 col-lg-3 mt-lg-5 pt-lg-5 col-md-3 mt-md-5 pt-md-5    h4">Oferta</li>
+                        <li class="col-xxl-2 mt-xxl-5 pt-xxl-5 col-xl-2 mt-xl-5 pt-xl-5 col-lg-3 mt-lg-5 pt-lg-5 col-md-3 mt-md-5 pt-md-5   h4">Cennik</li>
+                        <li class="col-xxl-2 mt-xxl-5 pt-xxl-5 col-xl-2 mt-xl-5 pt-xl-5 col-lg-3 mt-lg-5 pt-lg-5 col-md-3 mt-md-5 pt-md-5   h4">O nas</li>
+                        
                     </ul>
                 </section>
-                <!-- <div id="mainSite" class="col-xl-2 col-lg-2 float-start mt-xl-5 text-xl-center clearfix">Strona główna</div>
-                <div id="offer" class="col-xl-1 float-start mt-xl-5 text-xl-center clearfix " >Oferta</div>
-                <div id="prices" class="col-xl-1 float-start mt-xl-5 text-xl-center clearfix" >Cennik</div>
-                <div id="aboutUs" class="col-xl-1 float-start mt-xl-5 text-xl-center clearfix" >O nas</div> -->
-            <!-- </div> -->
-        <!-- </navbar> -->
-        <!-- <div id="container" class='clearfix'>
-            <div class=" col-xl-10 offset-xl-1  text-xl-center"></div>
+        <div id="siteContent" class='mt-5 h-xl-75 h-lg-75 h-md-50 container col-9 rounded '>
+           <p class='col-4 h5 text-center'>Translacje i Tłumaczenia Stanisław Hupert</p>
+           <div id="siteContentDescription">
+                <p class="offset-2 h5 mt-5 text-end">Nasza firma,z 40 letnią tradycją,zajmuje się profesjonalnymi tłumaczeniami języka chiński, dla firm,przedsiębiorców jak i dla prywatnych potrzeb.O tłumaczenie,można podać przez strone internetową,lub udać się do biura,pod wskazany adres. </p>
+           </div>
         </div>
         
         
-        <footer class="col-xl-10 offset-xl-1  text-xl-center clearfix"></footer> -->
+        <footer class="col-12  bg-warning  text-xl-center clearfix">
+        <div id="joinUs" >
+            <a href='./regulaminSerwisu'>
+                <b class="col-2 pe-5">Regulamin serwisu</b>
+            </a>
+            <a href='./politykaPrywatności'>
+                <b class="col-2 pe-5">Polityka Prywatności</b>
+            </a>
+            <b class="col-2 pe-5 clearfix">Ustawienia cookies</b>
+            <b class="col-3 offset-3 text-center">© 2001-2022 Stanisław Hupert Wszelkie prawa zastrzeżone. CA</b>
+            <b class="col-3 offset-2">Dołącz do nas:</b>
+            <a href="https://www.reddit.com">
+                <svg xmlns="http://www.w3.org/2000/svg" class='icon' fill="currentColor" class="bi bi-reddit" viewBox="0 0 16 16">
+                    <path d="M6.167 8a.831.831 0 0 0-.83.83c0 .459.372.84.83.831a.831.831 0 0 0 0-1.661zm1.843 3.647c.315 0 1.403-.038 1.976-.611a.232.232 0 0 0 0-.306.213.213 0 0 0-.306 0c-.353.363-1.126.487-1.67.487-.545 0-1.308-.124-1.671-.487a.213.213 0 0 0-.306 0 .213.213 0 0 0 0 .306c.564.563 1.652.61 1.977.61zm.992-2.807c0 .458.373.83.831.83.458 0 .83-.381.83-.83a.831.831 0 0 0-1.66 0z"/>
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.828-1.165c-.315 0-.602.124-.812.325-.801-.573-1.9-.945-3.121-.993l.534-2.501 1.738.372a.83.83 0 1 0 .83-.869.83.83 0 0 0-.744.468l-1.938-.41a.203.203 0 0 0-.153.028.186.186 0 0 0-.086.134l-.592 2.788c-1.24.038-2.358.41-3.17.992-.21-.2-.496-.324-.81-.324a1.163 1.163 0 0 0-.478 2.224c-.02.115-.029.23-.029.353 0 1.795 2.091 3.256 4.669 3.256 2.577 0 4.668-1.451 4.668-3.256 0-.114-.01-.238-.029-.353.401-.181.688-.592.688-1.069 0-.65-.525-1.165-1.165-1.165z"/>
+                </svg>
+            </a>
+            <a href="https://pl-pl.facebook.com">
+                <svg xmlns="http://www.w3.org/2000/svg" class='icon'  fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                </svg>
+            </a>
+            <a href="https://twitter.com/?lang=pl">
+                <svg xmlns="http://www.w3.org/2000/svg" class='icon'  fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                    <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+                </svg>
+            </a>
+        </div>
+       
+            
+        
+        </footer> 
 
     </body>
-            </html>
-<?php
-    echo '
-    ';
-?>
+</html>
+
