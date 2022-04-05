3fb9a1a012f02431e81e0fe63b399b2f2b57ed1a
Stanislaw <stashupert@gmail.com>
2022-04-05 22:03:36 +0200

    Zaczęto dopracowywać zawartość strony głównej i footera,i rozpoczęto prace nad ich responsywnością

--- index.php
+++ index.php
@@ -30,26 +30,31 @@
                         
                     </ul>
                 </section>
-        <div id="siteContent" class='mt-5 h-xl-75 h-lg-75 h-md-50 container col-9 rounded '>
-           <p class='col-4 h5 text-center'>Translacje i Tłumaczenia Stanisław Hupert</p>
-           <div id="siteContentDescription">
-                <p class="offset-2 h5 mt-5 text-end">Nasza firma,z 40 letnią tradycją,zajmuje się profesjonalnymi tłumaczeniami języka chiński, dla firm,przedsiębiorców jak i dla prywatnych potrzeb.O tłumaczenie,można podać przez strone internetową,lub udać się do biura,pod wskazany adres. </p>
+        <div id="siteContent" class='mt-5 h-xxl-75 h-lg-23 h-md-50 container col-9 rounded '>
+          <p class='col-xxl-4 h2 text-center mt-2'>Translacje i Tłumaczenia Stanisław Hupert</p>
+           <div class='m-2 rounded' id="siteContentDescription" >
+           <p class='col-xxl-4 h3-xxl text-center text-decoration-underline mt-2'>Kim jesteśmy?</p>
+                <p class="offset-xxl-2 h5 mt-xxl-5 text-end">Nasza firma,z 40 letnią tradycją,zajmuje się profesjonalnymi tłumaczeniami języka chiński, dla firm,przedsiębiorców jak i dla prywatnych potrzeb.O tłumaczenie,można podać przez strone internetową,lub udać się do biura,pod wskazany adres.Nasi tłumacze są absolwentami synologii,wielu dobrych uniewrsytetów i przebywali na terenie Chin niejednokrotnie.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare efficitur velit, vel imperdiet orci auctor nec. Praesent at facilisis mauris. Fusce viverra urna vitae mi consequat tincidunt. Phasellus eget hendrerit arcu, vitae volutpat dui. Pellentesque eget porta justo. Quisque eleifend iaculis congue. Fusce aliquet nisl dolor, elementum consectetur sem porttitor id. Ut pharetra lorem non quam egestas sodales. Suspendisse potenti.
+
+Suspendisse convallis elit vel lectus ultricies maximus. Duis tempus sodales lacinia. Sed varius a turpis sit amet tincidunt. Sed non quam viverra, dictum nisl vel, efficitur ligula. Vestibulum non quam egestas, molestie quam et, accumsan neque. Maecenas sed vulputate mauris. Curabitur non tellus arcu. Sed egestas rutrum nisi id porttitor. Aliquam eget efficitur elit, porta dignissim tortor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce malesuada eget arcu ut bibendum. Nulla cursus dui sit amet erat iaculis sodales. Mauris felis lorem, pharetra vitae metus at, dictum interdum purus. Nullam eu mattis nulla. Quisque et tincidunt lacus, id tincidunt lorem. Donec iaculis lacus ut nulla faucibus, ac cursus elit dapibus.Aenean ac lorem vestibulum, lacinia dolor vitae, ornare libero. Vivamus et nulla ut felis rutrum gravida. Praesent hendrerit dolor eu urna dictum, a elementum felis ultricies. Fusce et eros tellus. Morbi vehicula sem sem, ut feugiat magna volutpat eu. Curabitur congue ex lectus, maximus vestibulum libero tincidunt et. Integer euismod porta velit non bibendum. Donec semper diam tortor, at ornare lacus facilisis sed. Etiam vehicula nec dolor sit amet tristique. </p>
            </div>
+           
+           
         </div>
         
         
         <footer class="col-12  bg-warning  text-xl-center clearfix">
         <div id="joinUs" >
             <a href='./regulaminSerwisu'>
-                <b class="col-2 pe-5">Regulamin serwisu</b>
+                <b class="col-xxl-2 pe-xxl-5 col-xl-2 pe-xl-5  col-lg-2 pe-lg-5 ps-lg-5 col-md-2 pe-md-5 ps-md-5 ">Regulamin serwisu</b>
             </a>
             <a href='./politykaPrywatności'>
-                <b class="col-2 pe-5">Polityka Prywatności</b>
+                <b class="col-xxl-2 pe-xxl-5 col-xl-2 pe-xl-5 col-lg-2 pe-lg-5 col-md-2 pe-md-5">Polityka Prywatności</b>
             </a>
-            <b class="col-2 pe-5 clearfix">Ustawienia cookies</b>
-            <b class="col-3 offset-3 text-center">© 2001-2022 Stanisław Hupert Wszelkie prawa zastrzeżone. CA</b>
-            <b class="col-3 offset-2">Dołącz do nas:</b>
-            <a href="https://www.reddit.com">
+            <b class="col-xxl-2 pe-xxl-5 col-xl-2 pe-xl-5 col-lg-2 pe-lg-5 col-md-2 pe-md-5 clearfix">Ustawienia cookies</b>
+            <b class="col-xxl-3 offset-xxl-3 col-xl-3 offset-xl-3 col-lg-2  ps-lg-3 col-md-2 pe-md-2   text-center">© 2001-2022 Stanisław Hupert Wszelkie prawa zastrzeżone. CA</b>
+            <b class="col-xxl-3 offset-xxl-2 col-xl-1 col-lg-2 col-md-1 ps-md-2   offset-lg-1 display-md-none">Dołącz do nas:</b>
+            <a href="https://www.reddit.com" class=''>
                 <svg xmlns="http://www.w3.org/2000/svg" class='icon' fill="currentColor" class="bi bi-reddit" viewBox="0 0 16 16">
                     <path d="M6.167 8a.831.831 0 0 0-.83.83c0 .459.372.84.83.831a.831.831 0 0 0 0-1.661zm1.843 3.647c.315 0 1.403-.038 1.976-.611a.232.232 0 0 0 0-.306.213.213 0 0 0-.306 0c-.353.363-1.126.487-1.67.487-.545 0-1.308-.124-1.671-.487a.213.213 0 0 0-.306 0 .213.213 0 0 0 0 .306c.564.563 1.652.61 1.977.61zm.992-2.807c0 .458.373.83.831.83.458 0 .83-.381.83-.83a.831.831 0 0 0-1.66 0z"/>
                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.828-1.165c-.315 0-.602.124-.812.325-.801-.573-1.9-.945-3.121-.993l.534-2.501 1.738.372a.83.83 0 1 0 .83-.869.83.83 0 0 0-.744.468l-1.938-.41a.203.203 0 0 0-.153.028.186.186 0 0 0-.086.134l-.592 2.788c-1.24.038-2.358.41-3.17.992-.21-.2-.496-.324-.81-.324a1.163 1.163 0 0 0-.478 2.224c-.02.115-.029.23-.029.353 0 1.795 2.091 3.256 4.669 3.256 2.577 0 4.668-1.451 4.668-3.256 0-.114-.01-.238-.029-.353.401-.181.688-.592.688-1.069 0-.65-.525-1.165-1.165-1.165z"/>
